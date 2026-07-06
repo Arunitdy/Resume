16cd361cc854cf6c1617832fd1fb58a1e13363f9
Arun <arunmundakkal003@gmail.com>
2026-07-06 16:23:22 +0530

    feat: add TestController with a test endpoint

--- server/src/main/java/com/server/server/TestController.java
+++ server/src/main/java/com/server/server/TestController.java
@@ -1,10 +1,15 @@
-package com.server;
+package com.server.server;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello! Backend is running successfully!";
+    }
 
     @GetMapping("/test")
     public String test() {
